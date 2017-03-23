4b9082e7061a33fb09844d140b699fe915d26808
bthdimension <benjamin.buergisser@gmx.ch>
2017-03-24 00:50:24 +0100

    some experiments

--- ase_compute/main.go
+++ ase_compute/main.go
@@ -1,13 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/cdipaolo/sentiment"
 	"github.com/gin-gonic/gin"
+
 	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
 )
 
+var (
+	model sentiment.Models
+)
+
+func sentimentCdipaolo(sentence string) uint8 {
+	// fmt.Println("cdipaolo: ", model.SentimentAnalysis(sentence, sentiment.English).Score)
+	return model.SentimentAnalysis(sentence, sentiment.English).Score
+}
+
+// Tweet Text and sentiment value of a tweet
+type Tweet struct {
+	Text      string
+	Sentiment uint8
+}
+
 func main() {
+
+	var err error
+
+	// sentiment analysis
+	model, err = sentiment.Restore()
+	if err != nil {
+		panic(fmt.Sprintf("Could not restore model!\n\t%v\n", err))
+	}
+
+	// mongo
+	session, err := mgo.Dial("mongodb://127.0.0.1:27017")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	collection := session.DB("test").C("tweets")
+
+	// result := Tweet{}
+	// err = collection.Find(bson.M{"sentiment": 1}).One(&result)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println("Text:", result.Text)
+
+	// gin
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
@@ -16,5 +61,22 @@ func main() {
 		})
 	})
 
+	r.POST("/insert", func(c *gin.Context) {
+
+		message := c.Query("message")
+		sentimentValue := sentimentCdipaolo(message)
+		err = collection.Insert(&Tweet{message, sentimentValue})
+		if err != nil {
+			c.JSON(http.StatusConflict, gin.H{
+				"message": "Insert failed: " + message,
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Insert successful: " + message,
+			})
+		}
+	})
+
 	r.Run()
+
 }
