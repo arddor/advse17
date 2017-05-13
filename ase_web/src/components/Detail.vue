e4bb4fc5cf272ea15e1de89c86652d695f4d4fe9
Marc Heimgartner <marc.heimgartner@gmx.ch>
2017-05-13 11:43:22 +0200

    Added Plot

--- ase_web/src/components/Detail.vue
+++ ase_web/src/components/Detail.vue
@@ -1,28 +1,67 @@
 <template>
-  <div class="section" v-if="term">
-    <h1 class="title">{{term.term}}</h1>
-  </div>
-  <div class="section" v-else>
-    <p>Ups something went wrong</p>
-  </div>
+  <section class="hero is-medium" v-if="loading">
+    <div class="hero-body">
+      <div class="container">
+        <div class="column is-one-third is-offset-one-third has-text-centered">
+          <pulse-loader :loading="loading"></pulse-loader>
+          <h1 class="title">loading</h1>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="section" v-else>
+    <h1 class="title" v-if="term">{{term}}</h1>
+    <plotly
+      v-if="chart"
+      :data="chart"
+      :layout="{barmode: 'relative'}"
+    ></plotly>
+  </section>
 </template>
 
 <script>
 import axios from 'axios'
+import Plotly from '@/components/Plotly'
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
 export default {
-  async beforeRouteEnter (to, from, next) {
+  components: {
+    Plotly,
+    PulseLoader
+  },
+  async created () {
+    let {id} = this.$route.params
+    this.loading = true
     try {
-      let {data} = await axios.get(`api/terms/${to.params.id}`)
-      next(vm => {
-        vm.$data.term = data
-      })
+      let {data} = await axios.get(`api/terms/${id}`)
+      this.term = data.term
+      this.chart = this.chartData(data.data)
     } catch (e) {
-      next()
+      console.log(e)
     }
+    this.loading = false
   },
   data () {
     return {
-      term: null
+      loading: false,
+      term: null,
+      chart: null
+    }
+  },
+  methods: {
+    chartData (data) {
+      let positive = {x: [], y: [], type: 'bar', name: 'positive'}
+      let negative = {x: [], y: [], type: 'bar', name: 'negative'}
+
+      data.forEach(item => {
+        if (item.sentiment === 1) {
+          positive.x.push(new Date(item.time))
+          positive.y.push(1)
+        } else if (item.sentiment === 0) {
+          negative.x.push(new Date(item.time))
+          negative.y.push(-1)
+        }
+      })
+      return [positive, negative]
     }
   }
 }
