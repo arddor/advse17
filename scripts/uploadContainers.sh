a03a01c915eedeee3ab37289af4543a44f2fc4f4
Simon Tännler <simon@dhcp-wlan-uzh-10-12-248-024.uzh.ch>
2017-05-15 15:20:26 +0200

    adding some scripts

---
+++ scripts/uploadContainers.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+
+gcloud config set project advse-167708
+
+gcloud config list
+
+PROJECT_ID="$(gcloud config get-value project)"
+COMMIT_HASH="$(git rev-parse HEAD)"
+
+
+
+if [[ ${PROJECT_ID} == "advse-167708" ]]; then
+    docker build -t gcr.io/${PROJECT_ID}/ase_api:git_${COMMIT_HASH} ./ase_api/
+    docker build -t gcr.io/${PROJECT_ID}/ase_compute:git_${COMMIT_HASH} ./ase_compute/
+    docker build -t gcr.io/${PROJECT_ID}/ase_twitter:git_${COMMIT_HASH} ./ase_twitter/
+    docker build -t gcr.io/${PROJECT_ID}/ase_web:git_${COMMIT_HASH} ./ase_web/
+    
+    echo "##################### IMAGES #####################"
+    docker images | grep "git_"
+    echo "##################### BUILD COMPLETE, PUSHING... #####################"
+    
+    
+    gcloud docker -- push gcr.io/${PROJECT_ID}/ase_api:git_${COMMIT_HASH}
+    gcloud docker -- push gcr.io/${PROJECT_ID}/ase_compute:git_${COMMIT_HASH}
+    gcloud docker -- push gcr.io/${PROJECT_ID}/ase_twitter:git_${COMMIT_HASH}
+    gcloud docker -- push gcr.io/${PROJECT_ID}/ase_web:git_${COMMIT_HASH}
+
+    
+    echo "##################### PUSH COMPLETE #####################"
+
+    gcloud container images list
+
+    
+    
+    
+    
+    
+    
+fi
+
