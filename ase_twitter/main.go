c02f2e4b5c1bf4d717f34fc1481f8497e1cb0d9d
Marc Heimgartner <marc.heimgartner@gmx.ch>
2017-05-18 16:49:19 +0200

    Import db module from github

--- ase_twitter/main.go
+++ ase_twitter/main.go
@@ -3,13 +3,14 @@
 package main
 
 import (
-	"ase_api/db"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/arddor/advse17/ase_api/db"
 
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
@@ -46,7 +47,7 @@ func paramAlreadyTracked(param string) bool {
 			return true
 		}
 	}
-return false
+	return false
 }
 
 func removeTrackingParam(param string) {
@@ -74,71 +75,69 @@ func connectToMQ() *amqp.Connection {
 	}
 }
 
-
-
 func failOnError(err error, msg string) {
-  if err != nil {
-    log.Fatalf("%s: %s", msg, err)
-    fmt.Sprintf("%s: %s", msg, err)
-	fmt.Print("FailOnError")
-  }
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		fmt.Sprintf("%s: %s", msg, err)
+		fmt.Print("FailOnError")
+	}
 }
 
 func main() {
 
-// authenticate
-// TODO: change this so the keys are not in clear text
-consumerKey := "TheYSOyWqkVy5LS4AFj10LrXy"
-consumerSecret := "Qf9ovSx4aqFK9NkycjD2q1YYos5VhNVcNUFjyUjhDY8x3PWHoP"
-accessToken := "49389452-QjuTHd6wbDJUnRsD8gRbEPN076QVLlTVtHirbtgBa"
-accessSecret := "MlUhiDtWbYtMa1w3xLERmcATc6WVXYRr69xKGmnpslsWt"
+	// authenticate
+	// TODO: change this so the keys are not in clear text
+	consumerKey := "TheYSOyWqkVy5LS4AFj10LrXy"
+	consumerSecret := "Qf9ovSx4aqFK9NkycjD2q1YYos5VhNVcNUFjyUjhDY8x3PWHoP"
+	accessToken := "49389452-QjuTHd6wbDJUnRsD8gRbEPN076QVLlTVtHirbtgBa"
+	accessSecret := "MlUhiDtWbYtMa1w3xLERmcATc6WVXYRr69xKGmnpslsWt"
 
-config := oauth1.NewConfig(consumerKey, consumerSecret)
-token := oauth1.NewToken(accessToken, accessSecret)
-httpClient := config.Client(oauth1.NoContext, token)
+	config := oauth1.NewConfig(consumerKey, consumerSecret)
+	token := oauth1.NewToken(accessToken, accessSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
 
-//rabbitCloseError chan *amqp.Error
-//rabbitCloseError = make(chan *amqp.Error)
+	//rabbitCloseError chan *amqp.Error
+	//rabbitCloseError = make(chan *amqp.Error)
 
-var err error
-var conn *amqp.Connection
+	var err error
+	var conn *amqp.Connection
 
-connError := make(chan *amqp.Error)
-go func(){
-err := <- connError
-log.Println("reconnect: " + err.Error())
-conn = connectToMQ()
-}()
+	connError := make(chan *amqp.Error)
+	go func() {
+		err := <-connError
+		log.Println("reconnect: " + err.Error())
+		conn = connectToMQ()
+	}()
 
-// connect to RabbitMQ server
-conn = connectToMQ()
-failOnError(err, "Failed to connect to RabbitMQ")
-conn.NotifyClose(connError)
+	// connect to RabbitMQ server
+	conn = connectToMQ()
+	failOnError(err, "Failed to connect to RabbitMQ")
+	conn.NotifyClose(connError)
 
-// create a channel
-ch, err := conn.Channel()
-failOnError(err, "Failed to open a channel")
-defer conn.Close()
+	// create a channel
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer conn.Close()
 
-// declare a queue for us to send to
-q, err := ch.QueueDeclare(
-  "tweet", // name
-  true,   // durable -> queue is not "lost" even when rabbitMQ crashes
-  false,   // delete when unused
-  false,   // exclusive
-  false,   // no-wait
-  nil,     // arguments
-)
-failOnError(err, "Failed to declare a queue")
-defer ch.Close()
+	// declare a queue for us to send to
+	q, err := ch.QueueDeclare(
+		"tweet", // name
+		true,    // durable -> queue is not "lost" even when rabbitMQ crashes
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	defer ch.Close()
 
-var stream *twitter.Stream
+	var stream *twitter.Stream
 
-client := twitter.NewClient(httpClient)
+	client := twitter.NewClient(httpClient)
 
-db.Initialize("ase_timeseries:28015")
+	db.Initialize("ase_timeseries:28015")
 
-// Convenience Demux demultiplexed stream messages
+	// Convenience Demux demultiplexed stream messages
 	demux := twitter.NewSwitchDemux()
 	demux.Tweet = func(tweet *twitter.Tweet) {
 
@@ -160,7 +159,7 @@ db.Initialize("ase_timeseries:28015")
 			})
 		failOnError(err, "Failed to publish a message")
 	}
-	
+
 	var terms []db.Term
 	terms, error := db.GetTerms(false)
 
@@ -186,46 +185,46 @@ db.Initialize("ase_timeseries:28015")
 	// Is there a need to implement a 'quit' handle in the routine?
 	go demux.HandleChan(stream.Messages)
 
-// This gets called a lot due to inserts of sentiments
-db.OnChange(func(change map[string]*db.Term) {
-	var tempTerm *db.Term 
-	var oldTerm *db.Term
-	tempTerm = change["new_val"]
-	oldTerm = change["old_val"]
-	fmt.Println( "Change: " )
-	if oldTerm != tempTerm {
-	// TODO: check conditions
+	// This gets called a lot due to inserts of sentiments
+	db.OnChange(func(change map[string]*db.Term) {
+		var tempTerm *db.Term
+		var oldTerm *db.Term
+		tempTerm = change["new_val"]
+		oldTerm = change["old_val"]
+		fmt.Println("Change: ")
+		if oldTerm != tempTerm {
+			// TODO: check conditions
 
-		if tempTerm == nil {
-			removeTrackingParam(oldTerm.Term)
-			fmt.Println(oldTerm.Term + " deleted." )
-		}
-		
-		if (oldTerm == nil && tempTerm != nil) || tempTerm != nil {
-			if paramAlreadyTracked(tempTerm.Term) {
-				return
+			if tempTerm == nil {
+				removeTrackingParam(oldTerm.Term)
+				fmt.Println(oldTerm.Term + " deleted.")
 			}
-			
+
+			if (oldTerm == nil && tempTerm != nil) || tempTerm != nil {
+				if paramAlreadyTracked(tempTerm.Term) {
+					return
+				}
+
 				addTrackingParam(tempTerm.Term)
-				fmt.Println(tempTerm.Term + " added." )
-			
+				fmt.Println(tempTerm.Term + " added.")
+
+			}
+
+			stream.Stop()
+			params := &twitter.StreamFilterParams{
+				Track:         trackingParams,
+				StallWarnings: twitter.Bool(true),
+			}
+			stream, err = client.Streams.Filter(params)
+			// @marc: TODO: can this be handled in that way or do I have to somehow close the opened
+			// go routine and open a new one?
+			//go demux.HandleChan(stream.Messages)
+			demux.HandleChan(stream.Messages)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
-		
-		stream.Stop()
-		params := &twitter.StreamFilterParams{
-			Track: trackingParams,
-			StallWarnings: twitter.Bool(true),
-		}
-		stream, err = client.Streams.Filter(params)
-		// @marc: TODO: can this be handled in that way or do I have to somehow close the opened
-		// go routine and open a new one?
-		//go demux.HandleChan(stream.Messages)
-		demux.HandleChan(stream.Messages)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-})
+	})
 
 	// Wait for SIGINT and SIGTERM (HIT CTRL-C)
 	channel := make(chan os.Signal)
