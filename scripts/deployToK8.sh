5f9111b2fda3f01c9d7afc4cb5c967f22ac741b7
Simon Tännler <simon@mbp-simon.fritz.box>
2017-05-18 21:19:30 +0200

    scripts for k8

--- scripts/deployToK8.sh
+++ scripts/deployToK8.sh
@@ -20,7 +20,7 @@ kubectl create -f ./ase_api/api.yaml;
 kubectl create -f ./ase_api/api_service.yaml;
 
 
-kubectl delete -f ./ase_api/api.yaml; kubectl delete -f ./ase_api/api_service.yaml; kubectl create -f ./ase_api/api.yaml; kubectl create -f ./ase_api/api_service.yaml;
+#kubectl delete -f ./ase_api/api.yaml; kubectl delete -f ./ase_api/api_service.yaml; kubectl create -f ./ase_api/api.yaml; kubectl create -f ./ase_api/api_service.yaml;
 
 
 
@@ -31,7 +31,7 @@ kubectl delete -f ./ase_compute/compute.yaml;
 kubectl create -f ./ase_compute/compute.yaml;
 
 
-kubectl delete -f ./ase_compute/compute.yaml; kubectl create -f ./ase_compute/compute.yaml;
+#kubectl delete -f ./ase_compute/compute.yaml; kubectl create -f ./ase_compute/compute.yaml;
 
 
 
@@ -45,7 +45,7 @@ kubectl create -f ./ase_queue/queue.yaml;
 kubectl create -f ./ase_queue/queue_service.yaml;
 
 
-kubectl delete -f ./ase_queue/queue.yaml; kubectl delete -f ./ase_queue/queue_service.yaml; kubectl create -f ./ase_queue/queue.yaml; kubectl create -f ./ase_queue/queue_service.yaml;
+#kubectl delete -f ./ase_queue/queue.yaml; kubectl delete -f ./ase_queue/queue_service.yaml; kubectl create -f ./ase_queue/queue.yaml; kubectl create -f ./ase_queue/queue_service.yaml;
 
 
 
@@ -60,7 +60,7 @@ kubectl create -f ./ase_timeseries/timeseries_service_admin.yaml
 kubectl create -f ./ase_timeseries/timeseries_service_db.yaml
 
 
-kubectl delete -f ./ase_timeseries/timeseries.yaml; kubectl delete -f ./ase_timeseries/timeseries_service_admin.yaml; kubectl delete -f ./ase_timeseries/timeseries_service_db.yaml; kubectl create -f ./ase_timeseries/timeseries.yaml; kubectl create -f ./ase_timeseries/timeseries_service_admin.yaml; kubectl create -f ./ase_timeseries/timeseries_service_db.yaml;
+#kubectl delete -f ./ase_timeseries/timeseries.yaml; kubectl delete -f ./ase_timeseries/timeseries_service_admin.yaml; kubectl delete -f ./ase_timeseries/timeseries_service_db.yaml; kubectl create -f ./ase_timeseries/timeseries.yaml; kubectl create -f ./ase_timeseries/timeseries_service_admin.yaml; kubectl create -f ./ase_timeseries/timeseries_service_db.yaml;
 
 
 
@@ -71,7 +71,7 @@ kubectl delete -f ./ase_twitter/twitter.yaml;
 kubectl create -f ./ase_twitter/twitter.yaml;
 
 
-kubectl delete -f ./ase_twitter/twitter.yaml; kubectl create -f ./ase_twitter/twitter.yaml;
+#kubectl delete -f ./ase_twitter/twitter.yaml; kubectl create -f ./ase_twitter/twitter.yaml;
 
 
 
@@ -85,7 +85,7 @@ kubectl create -f ./ase_web/web.yaml;
 kubectl create -f ./ase_web/web_service.yaml;
 
 
-kubectl delete -f ./ase_web/web.yaml; kubectl delete -f ./ase_web/web_service.yaml; kubectl create -f ./ase_web/web.yaml; kubectl create -f ./ase_web/web_service.yaml;
+#kubectl delete -f ./ase_web/web.yaml; kubectl delete -f ./ase_web/web_service.yaml; kubectl create -f ./ase_web/web.yaml; kubectl create -f ./ase_web/web_service.yaml;
 
 
 
---
+++ scripts/statusK8.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+
+kubectl get service
+kubectl get deployment
+kubectl get pod
