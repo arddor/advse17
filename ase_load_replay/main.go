d8e2b0a05778adf15adddac052e37cad36a1b341
Simon Tännler <simon@mbp-simon.fritz.box>
2017-05-21 21:11:55 +0200

    ase_load_replay

--- ase_load_replay/main.go
+++ ase_load_replay/main.go
@@ -8,18 +8,23 @@ import (
 	
 	// https://labix.org/mgo
 	"gopkg.in/mgo.v2"
-	//"gopkg.in/mgo.v2/bson"
+	"gopkg.in/mgo.v2/bson"
 	
 	"github.com/streadway/amqp"
 )
 
-var amqpChannel *amqp.Channel
+type Tweet struct {
+	Id		bson.ObjectId `json:"id" bson:"_id"`
+	Text		string        `json:"text" bson:"text"`
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		fmt.Sprintf("%s: %s", msg, err)
+	}
+}
 
 func main() {
-	
-	// RabbitQ
-	fmt.Println("Connecting to AMQP...")
-	initializeAMQPConnection()
 	
 	// mongo
 	fmt.Println("Connecting to mongodb...")
@@ -30,91 +35,65 @@ func main() {
 	}
 	defer session.Close()
 	
-	collection := session.DB("test").C("trump_tweets")
+	coll := session.DB("test").C("trump_tweets")
 	
+	// RabbitQ
+	fmt.Println("Connecting to RabbitQ...")
 	
-	
-	body := "Hi I like Hamburgers"
-	timestamp := time.Now().String()
-	
-	err := amqpChannel.Publish(
-			"",     // exchange
-			"tweet", // routing key
-			false,  // mandatory
-			false,  // immediate
-			amqp.Publishing{
-				DeliveryMode: amqp.Persistent,
-				ContentType:  "text/plain",
-				MessageId:    timestamp,
-				Body:         []byte(body),
-			})
-	if err != nil {
-		fmt.Println("Error while publishing tweet: ", err)
-	}
-
-
-	fmt.Println("Exiting replay...")
-}
-
-func initializeAMQPConnection() {
-	var err error
 	var conn *amqp.Connection
-	var ch *amqp.Channel
-
-	connError := make(chan *amqp.Error)
-	go func() {
-		err := <-connError
-		fmt.Println("reconnect: ", err)
-		conn, ch = connectToMQ()
-		//TODO: is this neccessary?
-		//conn.NotifyClose(connError)
-	}()
-
-	conn, amqpChannel = connectToMQ()
-	conn.NotifyClose(connError)
-
-	defer conn.Close()
-
-	if(amqpChannel == nil){
-		amqpChannel, err = conn.Channel()
-		fmt.Println("Failed to open a channel: ", err)
+	var connectError error
+	
+	for {
+		conn, connectError = amqp.Dial("amqp://queue:5672")
+		if connectError == nil {
+			break
+		} else {
+			time.Sleep(1000 * time.Millisecond)
+		}
 	}
 
-	q, err := amqpChannel.QueueDeclare(
+	fmt.Println("Queue connected")
+	defer conn.Close()
+	
+	// create a channel
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+	
+	q, err := ch.QueueDeclare(
 		"tweet", // name
-		true,    // durable -> queue is not "lost" even when rabbitMQ crashes
+		true,   // durable
 		false,   // delete when unused
 		false,   // exclusive
 		false,   // no-wait
 		nil,     // arguments
 	)
-	if err != nil {
-		fmt.Println("Error while creating a Queue: ", err)
-	}
+	failOnError(err, "Failed to declare a queue")
 	fmt.Println("Declared queue " + q.Name)
-}
-
-func connectToMQ() (*amqp.Connection, *amqp.Channel) {
-	for {
-		var conn *amqp.Connection
-		var err error
-		conn, err = amqp.Dial("amqp://queue:5672")
-		if err == nil {
-			for {
-				channel, err := conn.Channel()
-				if err == nil{
-					// TODO: are those next 2 lines neccessary?
-					defer conn.Close()
-					defer channel.Close()
-
-					return conn, channel
-				}
-				fmt.Println("Error with channel creation: ", err)
-			}
-		}
-		// else, reconnect after timeout
-		time.Sleep(1000 * time.Millisecond)
-		fmt.Println("Reconnect to AMQP...")
+	
+	// loop through all items
+	
+	result := Tweet{}
+		
+	iter := coll.Find(nil).Batch(10000).Iter()
+	for iter.Next(&result) {		
+		err = ch.Publish(
+		  "",		// exchange
+		  "tweet",	// routing key
+		  false,		// mandatory
+		  false,		// immediate
+		  amqp.Publishing {
+			DeliveryMode: amqp.Persistent,
+			ContentType:  "text/plain",
+			MessageId:    time.Now().Format("Mon Jan 02 15:04:05 +0000 2006").String(),
+			Body:         []byte(result.Text),
+		  })
+		failOnError(err, "Failed to publish a message")
 	}
+	
+	if err = iter.Close(); err != nil {
+		failOnError(err, "Failed to close iterator")
+	}
+	
+	fmt.Println("Exiting replay...")
 }
-
