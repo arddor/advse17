54b1541ad1255c4a3f47ed2b0c386906343233c6
Simon Tännler <simon@dhcp-wlan-uzh-10-12-127-090.uzh.ch>
2017-05-22 12:05:01 +0200

    using alpine

---
+++ scripts/k8applyAll.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+
+
+
+#gcloud auth application-default login
+
+#gcloud container clusters get-credentials awesome-cluster --zone europe-west1-c --project advse-167708
+
+#kubectl proxy
+
+
+############################## api
+
+kubectl apply -f ./ase_api/api.yaml;
+kubectl apply -f ./ase_api/api_service.yaml;
+
+
+
+##################### compute
+
+kubectl apply -f ./ase_compute/compute.yaml;
+
+
+
+##################### replay
+
+kubectl apply -f ./ase_load_replay/replay.yaml;
+
+
+
+##################### tweets
+
+kubectl apply -f ./ase_load_tweets/tweets.yaml;
+kubectl apply -f ./ase_load_tweets/tweets_service.yaml;
+
+
+
+##################### queue
+
+kubectl apply -f ./ase_queue/queue.yaml;
+kubectl apply -f ./ase_queue/queue_service.yaml;
+
+
+
+############################## timeseries
+
+kubectl apply -f ./ase_timeseries/timeseries.yaml
+kubectl apply -f ./ase_timeseries/timeseries_service_admin.yaml
+kubectl apply -f ./ase_timeseries/timeseries_service_db.yaml
+
+
+
+##################### twitter
+
+kubectl apply -f ./ase_twitter/twitter.yaml;
+
+
+
+##################### web
+
+kubectl apply -f ./ase_web/web.yaml;
+kubectl apply -f ./ase_web/web_service.yaml;
+
+
+
