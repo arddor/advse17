5b2c7f16fa54a5d83ea84f16c80a7e0ac38b2594
Marc Heimgartner <marc.heimgartner@gmx.ch>
2017-05-25 11:13:31 +0200

    Forgot to revert database connection

--- ase_api/main.go
+++ ase_api/main.go
@@ -20,7 +20,7 @@ var upgrader = websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { retu
 
 func main() {
 	s := Server{}
-	s.Initialize(":28015")
+	s.Initialize("timeseries-db:28015")
 	s.Run(":8000")
 }
 
