1a2b06bc8ab57f8cdf2860467546054816b23c19
Marc Heimgartner <marc.heimgartner@gmx.ch>
2017-05-26 10:41:52 +0200

    Remove origin check for websocket

--- ase_api/main.go
+++ ase_api/main.go
@@ -17,7 +17,7 @@ import (
 
 // TODO: Add check of origin again
 // var upgrader = websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}
-var upgrader = &websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024}
+var upgrader = &websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024, CheckOrigin: func(r *http.Request) bool { return true }}
 
 func main() {
 	s := Server{}
